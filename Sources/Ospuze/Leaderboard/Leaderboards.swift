691d97fc42ee5756b4b9d559cf0eda3c46381b4b
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2023-12-30 20:26:51 +0100

    Update Leaderboards.swift

--- Sources/Ospuze/Leaderboard/Leaderboards.swift
+++ Sources/Ospuze/Leaderboard/Leaderboards.swift
@@ -2,56 +2,37 @@ import Foundation
 import YASU
 
 public struct Leaderboards {
-    static let CB1 = "https://embark-discovery-leaderboard.storage.googleapis.com/leaderboard-beta-1.json"
-    static let CB2 = "https://embark-discovery-leaderboard.storage.googleapis.com/leaderboard.json"
-    static let OB = "https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-crossplay.json"
-    static let S1 = "https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-crossplay-discovery-live.json"
+    
 }
 
 public extension Leaderboards {
-    enum identifiers {
-        case CB1, CB2, OB, S1
+    enum identifiers: String {
+        case ClosedBeta1 =      "https://embark-discovery-leaderboard.storage.googleapis.com/leaderboard-beta-1.json"
+        case ClosedBeta2 =      "https://embark-discovery-leaderboard.storage.googleapis.com/leaderboard.json"
+        case OpenBeta =         "https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-crossplay.json"
+        case LiveCrossplay =    "https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-crossplay-discovery-live.json"
+        case LivePSN =          "https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-psn-discovery-live.json"
+        case LiveXbox =         "https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-xbox-discovery-live.json"
+        case LiveSteam =        "https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-steam-discovery-live.json"
     }
     
-    enum archives {
-        case CB1, CB2, OB
+    enum archives: String {
+        case ClosedBeta1 =  "CB1.json"
+        case ClosedBeta2 =  "CB2.json"
+        case OpenBeta =     "OB.json"
     }
     
     /// attempt to get the current leaderboards
     /// - Parameter identifier: the desired leaderboard
     /// - Returns: an array of leaderboard entries?
     static func getLeaderboard(_ identifier: identifiers) async -> [LeaderboardEntry]? {
-        
-        var leaderboard = ""
-        switch identifier {
-        case .CB1:
-            leaderboard = CB1 //closed beta 1
-        case .CB2:
-            leaderboard = CB2 //closed beta 2
-        case .OB:
-            leaderboard = OB //open beta
-        case .S1:
-            leaderboard = S1 //season 1
-        }
-        
-        return await loadData(URL(string: leaderboard)!) as [LeaderboardEntry]?
+        return await loadData(URL(string: identifier.rawValue)!) as [LeaderboardEntry]?
     }
     
     /// get an archived version of the leaderboard for past seasons
     /// - Parameter archive: the desired leaderboard
     /// - Returns: an array of leaderboard entries
     static func getArchivedLeaderboard(_ archive: archives) async -> [LeaderboardEntry] {
-        var fileName = ""
-        
-        switch archive {
-        case .CB1:
-            fileName = "CB1.json"
-        case .CB2:
-            fileName = "CB2.json"
-        case .OB:
-            fileName = "OB.json"
-        }
-        
-        return Bundle.module.decode(fileName) as [LeaderboardEntry]
+        return Bundle.module.decode(archive.rawValue) as [LeaderboardEntry]
     }
 }
