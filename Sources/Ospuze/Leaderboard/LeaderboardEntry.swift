9bb24b82dc2336bc6ca863bf8b713c245e91a92f
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-01-29 01:13:06 +0100

    Update LeaderboardEntry.swift

--- Sources/Ospuze/Leaderboard/LeaderboardEntry.swift
+++ Sources/Ospuze/Leaderboard/LeaderboardEntry.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct LeaderboardEntry: Codable {
+public struct LeaderboardEntry: Codable, Hashable {
     public let r: Int //Rank
     public let name: String //EmbarkID
     public let f: Int //Fame
@@ -10,4 +10,19 @@ public struct LeaderboardEntry: Codable {
     public let steam: String?
     public let xbox: String?
     public let psn: String?
+}
+
+public extension LeaderboardEntry {
+    var rankChange: Int { or - r }
+    var fameChange: Int { of - f }
+    
+    var formattedRankChange: String { formattedChange(rankChange) }
+    var formattedFameChange: String { formattedChange(fameChange) }
+    
+    func formattedChange(_ input: Int) -> String {
+        let formatter = NumberFormatter()
+        formatter.positivePrefix = "+"
+        
+        return formatter.string(for: input) ?? ""
+    }
 }
