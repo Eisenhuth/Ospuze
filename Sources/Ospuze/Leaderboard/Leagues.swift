30461d3fd358332f8f1c48f1c2c3814954748e9a
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-03-17 22:00:18 +0100

    Season 2 (#1)
    * specified exact YASU version
    
    * added archives for Season 1
    
    * updated for Season 2
    
    * Update README.md

---
+++ Sources/Ospuze/Leaderboard/Leagues.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public struct Leagues{
+    
+    static let leaguesDict: [Int : String] = [
+        20: "Diamond 1",
+        19: "Diamond 2",
+        18: "Diamond 3",
+        17: "Diamond 4",
+        16: "Platinum 1",
+        15: "Platinum 2",
+        14: "Platinum 3",
+        13: "Platinum 4",
+        12: "Gold 1",
+        11: "Gold 2",
+        10: "Gold 3",
+        9: "Gold 4",
+        8: "Silver 1",
+        7: "Silver 2",
+        6: "Silver 3",
+        5: "Silver 4",
+        4: "Bronze 1",
+        3: "Bronze 2",
+        2: "Bronze 3",
+        1: "Bronze 4",
+    ]
+}
