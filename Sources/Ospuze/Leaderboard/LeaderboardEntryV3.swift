0501fe1730d348d93d073654cd2d0c09ff5d415a
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-06-17 03:53:17 +0200

    Season 3

--- Sources/Ospuze/Leaderboard/LeaderboardEntryV3.swift
+++ Sources/Ospuze/Leaderboard/LeaderboardEntryV3.swift
@@ -1,7 +1,24 @@
 import Foundation
 
-public struct LeaderboardEntryV3: Codable, Hashable {
+public struct LeaderboardV3: Codable, Hashable {
+    public let entries: [LeaderboardEntryV3]
     
+    enum CodingKeys: String, CodingKey {
+        case entries = "data"
+    }
+}
+
+public struct LeaderboardEntryV3: Codable, Hashable {
+    public let rank: Int
+    public let change: Int?
+    public let name: String
+    public let steamName: String
+    public let xboxName: String
+    public let psnName: String
+    public let leagueNumber: Int?
+    public let league: String?
+    public let rankScore: Int?
+    public let cashouts: Int?    
 }
 
 public extension LeaderboardEntryV3 {
